9378001d1a9f30548b7aa6a6d4757dd2654c597e
ayirp2706 <158231883+ayirp2706@users.noreply.github.com>
2025-10-25 15:57:10 +0530

    github

---
+++ maths.java
@@ -0,0 +1,117 @@
+public class maths {
+
+    public static void main (String [] args){
+/*int a =12589;
+int b=0;
+
+while(a!=0){
+
+    int tem = a%10;
+    b=(b*10)+tem;
+    a=a/10;
+
+System.out.print(b);*/
+
+// int n1=0,n2=1,n3
+
+/*for (int i=0; i<5; i++){
+
+    for(int j=0;j<i; j++){
+
+        System.out.print(" ");
+    }
+
+    for(int j=i; j<5; j++){
+
+System.out.print("*");
+
+    }
+
+System.out.println();*/ // inverted star pattern
+
+     // number pattern
+
+     /*  int i,j=1;
+     for (i=1;i<=10;i++){
+
+for(j=1;j<i+1;j++){
+
+System.out.print(j+" ");
+
+}
+ System.out.println();
+
+}*/
+
+// positve or neagitve number
+/*for (int i=5; i>=1;   i--)  
+ {
+
+   for (int j=1; j<=i; j++)
+   {
+
+      System.out.print("*");
+
+   }
+   System.out.println();
+
+
+ }*/
+
+
+
+ /*for (int i=0; i<=5; i++){
+
+System.out.print("*");
+ for (int j=5; j<i; j++){
+
+System.out.println();*/
+
+
+int a=-6;
+
+if(a>0)
+System.out.println("the number is postive");
+
+
+
+else if (a<0)
+System.out.println("the number is negative");
+
+else
+System.out.println("zero");
+
+
+ // even or odd number
+int b=4;
+
+if(b%2==0)
+ System.out.println("" + "num is even"); 
+
+ else
+ System.out.println(""+"num is odd");
+
+// sum of  n natural number
+int n= 12;
+ int sum =0;
+
+ for( int i=1; i<=n; i++)
+ sum+=i;
+ System.out.println(sum);
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+    
+    
+
